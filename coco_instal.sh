2f11c54f646c3c6200423b396e54ab73e6f4172e
Juanmacdonagh17 <46287032+Juanmacdonagh17@users.noreply.github.com>
2024-11-12 10:25:30 -0300

    Update coco_instal.sh

--- coco_instal.sh
+++ coco_instal.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+
+echo 'installing coconut and curl.h'
+
 # check for GCC
+
+echo 'checking for GCC'
+
 if ! [ -x "$(command -v gcc)" ]; then
-  echo 'Error: gcc is not installed.' >&2
+  echo 'error: gcc is not installed. install it an try again :)' >&2
   exit 1
 fi
 
@@ -11,6 +17,8 @@ mkdir -p $HOME/local
 cd $HOME/local
 
 # download and install libcurl
+echo 'downloding curl'
+
 wget https://curl.se/download/curl-7.88.1.tar.gz
 tar -xzvf curl-7.88.1.tar.gz
 cd curl-7.88.1
@@ -18,6 +26,8 @@ cd curl-7.88.1
 make && make install
 
 # compile
+echo 'compiling'
+
 gcc -o coconut /path/to/coconut.c -I$HOME/local/include -L$HOME/local/lib -lcurl
 
 echo 'Installation complete.'
